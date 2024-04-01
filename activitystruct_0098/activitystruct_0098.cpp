9a543b8051ff3ce5d39df0343e2bd7fccbe8cf0f
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-01 16:28:04 +0700

    Membuat input alamat mahasiswa pada fungsi main pada contoh 4

--- activitystruct_0098/activitystruct_0098.cpp
+++ activitystruct_0098/activitystruct_0098.cpp
@@ -128,28 +128,41 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa : ";
+		geatline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		geatline(cin, mhs[i].nama);
+
+		cout << "Alamat Mahasiswa : " << endl;
+		cout << "\t Nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama Kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur Mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa ke-" << (i + 1) << ":"<<endl;
+		cout << "\n NIM : " << mhs[i].nim;
+		cout << "\n Nama : " << mhs[i].nama;
+		cout << "\n Alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
 
 	}
-	cout << "Nomor Mahasiswa : ";
-	getline(cin, mhs.nim);
-	cout << "Nama Mahasiswa : ";
-	getline(cin, mhs.nama);
-
-	cout << "Alamat Mahasiswa : " << endl;
-	cout << "\t Nama Desa : ";
-	cin >> mhs.alamat.desa;
-	cout << "\t Nama Kota : ";
-	cin >> mhs.alamat.kota;
-
-	cout << endl;
-	cout << "\n NIM : " << mhs.nim;
-	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : ";
-	cout << "\n \t Desa : " << mhs.alamat.desa;
-	cout << "\n \t Kota : " << mhs.alamat.kota;
-	cout << "\n Umur : " << mhs.umur;
-
+		
 }
+	
+
+	
+
+	
 
 
 
